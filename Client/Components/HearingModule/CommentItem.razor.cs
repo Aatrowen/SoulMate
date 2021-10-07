c7d94820f2a9f1e6e6a0ced4865d7da6067c8b32
Aatrowen <1402210387@qq.com>
2021-10-07 22:46:34 +0800

    add some files

---
+++ Client/Components/HearingModule/CommentItem.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using SoulMate.Shared.HearingModule;
+
+namespace SoulMate.Client.Components.HearingModule
+{
+    public partial class CommentItem
+    {
+        [Parameter] public CommentItemDto CommentItemDto { get; set; } = new CommentItemDto();
+    }
+} 
---
+++ Shared/HearingModule/CommentItemDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SoulMate.Shared.HearingModule
+{
+    public class CommentItemDto
+    {
+        public DateTime DateTime { get; set; }
+
+        public string CommentContent { get; set; }
+    }
+}
